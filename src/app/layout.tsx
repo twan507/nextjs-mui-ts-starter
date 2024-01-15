e6ab78a7eed735b35f52e6fd0d972ac5a09c72f7
twan507 <tuanbuianh507@gmail.com>
2024-01-15 09:53:16 +0700

    98

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import AppFooter from '@/components/footer/app.footer';
 import AppHeader from '@/components/header/app.header';
 import ThemeRegistry from '@/components/theme-registry/theme.registry';
 
@@ -8,6 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ThemeRegistry>
           <AppHeader />
           {children}
+          <AppFooter />
         </ThemeRegistry>
       </body>
     </html>
