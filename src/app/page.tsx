f1a7cf72586f6fe4e75c97968d2fa221179576a8
“twan” <tuanbuianh507@gmail.com>
2024-01-16 09:54:34 +0700

    104

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,34 +3,35 @@ import { Container } from "@mui/material";
 import { sendRequest } from "@/utlis/api";
 export default async function HomePage() {
 
-  //   const res = await fetch('http://localhost:8000/api/v1/tracks/top', {
-  //     method: "POST",
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({
-  //       category: 'CHILL',
-  //       limit: 10
-  //     })
-  // })
-
-  // console.log(await res.json())
-
-
-  const res = await sendRequest<IBackendRes<ITrackTop[]>>({
+  const chill = await sendRequest<IBackendRes<ITrackTop[]>>({
     url: 'http://localhost:8000/api/v1/tracks/top',
     method: 'POST',
-    body: {
-      category: 'CHILL',
-      limit: 10
-    }
+    body: { category: 'CHILL', limit: 10 }
+  })
+
+  const workout = await sendRequest<IBackendRes<ITrackTop[]>>({
+    url: 'http://localhost:8000/api/v1/tracks/top',
+    method: 'POST',
+    body: { category: 'WORKOUT', limit: 10 }
+  })
+
+  const party = await sendRequest<IBackendRes<ITrackTop[]>>({
+    url: 'http://localhost:8000/api/v1/tracks/top',
+    method: 'POST',
+    body: { category: 'PARTY', limit: 10 }
   })
 
   return (
     <Container>
-      <MainSlider />
-      <MainSlider />
-      <MainSlider />
+      <MainSlider
+        title={"TOP CHILL"}
+        data={chill?.data ?? []} />
+      <MainSlider
+        title={"TOP WORKOUT"}
+        data={workout?.data ?? []} />
+      <MainSlider
+        title={"TOP PARTY"}
+        data={party?.data ?? []} />
     </Container>
   )
 }
