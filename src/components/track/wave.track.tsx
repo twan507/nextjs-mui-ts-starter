5ea71bd6571319b91d547ae2855e93c235da01f3
“twan” <tuanbuianh507@gmail.com>
2024-01-16 15:47:00 +0700

    121

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -4,8 +4,10 @@ import { useEffect, useRef, useState, useMemo, useCallback } from "react";
 import { useSearchParams } from 'next/navigation';
 import { useWavesurfer } from "@/utlis/customHook";
 import { WaveSurferOptions } from 'wavesurfer.js';
-import './wave.scss';
 import { Container } from "@mui/material";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
+import './wave.scss';
 
 const WaveTrack = () => {
     const searchParams = useSearchParams()
@@ -23,7 +25,7 @@ const WaveTrack = () => {
             const canvas = document.createElement('canvas');
             const ctx = canvas.getContext('2d')!;
             // Define the waveform gradient
-            gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1);
+            gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
             gradient.addColorStop(0, '#656666') // Top color
             gradient.addColorStop((canvas.height * 0.7) / canvas.height, '#656666') // Top color
             gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff') // White line
@@ -32,7 +34,7 @@ const WaveTrack = () => {
             gradient.addColorStop(1, '#B1B1B1') // Bottom color
 
             // Define the progress gradient
-            progressGradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1)
+            progressGradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35)
             progressGradient.addColorStop(0, '#EE772F') // Top color
             progressGradient.addColorStop((canvas.height * 0.7) / canvas.height, '#EB4926') // Top color
             progressGradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff') // White line
@@ -44,8 +46,8 @@ const WaveTrack = () => {
         return {
             waveColor: gradient,
             progressColor: progressGradient,
-            height: 150,
-            barWidth: 2,
+            height: 100,
+            barWidth: 3,
             url: `/api?audio=${fileName}`,
         }
     }, []);
@@ -55,8 +57,8 @@ const WaveTrack = () => {
     // Initialize wavesurfer when the container mounts
     // or any of the props change
     useEffect(() => {
-        if (!wavesurfer) return
-        setIsPlaying(false)
+        if (!wavesurfer) return;
+        setIsPlaying(false);
 
         const hover = hoverRef.current!;
         const waveform = containerRef.current!;
@@ -71,6 +73,9 @@ const WaveTrack = () => {
             wavesurfer.on('timeupdate', (currentTime) => {
                 setTime(formatTime(currentTime));
             }),
+            wavesurfer.once('interaction', () => {
+                wavesurfer.play()
+            })
         ]
 
         return () => {
@@ -93,17 +98,108 @@ const WaveTrack = () => {
     }
 
     return (
-        <Container style={{ marginTop: 100 }}>
-            <div ref={containerRef} className="wave-form-container">
-                <div className="time" >{time}</div>
-                <div className="duration" >{duration}</div>
-                <div ref={hoverRef} className="hover-wave"></div>
-            </div>
-            <button onClick={() => onPlayClick()}>
-                {isPlaying === true ? "Pause" : "Play"}
-            </button>
-        </Container>
+        <Container style={{ marginTop: 20 }}>
+            <div
+                style={{
+                    display: "flex",
+                    gap: 15,
+                    padding: 20,
+                    height: 400,
+                    background: "linear-gradient(135deg, rgb(106, 112, 67) 0%, rgb(11, 15, 20) 100%)"
+                }}
+            >
+                <div className="left"
+                    style={{
+                        width: "75%",
+                        height: "calc(100% - 10px)",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "space-between"
+                    }}
+                >
+                    <div className="info" style={{ display: "flex" }}>
+                        <div>
+                            <div
+                                onClick={() => onPlayClick()}
+                                style={{
+                                    borderRadius: "50%",
+                                    background: "#f50",
+                                    height: "50px",
+                                    width: "50px",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    cursor: "pointer"
+                                }}
+                            >
+                                {isPlaying === true ?
+                                    <PauseIcon
+                                        sx={{ fontSize: 30, color: "white" }}
+                                    />
+                                    :
+                                    <PlayArrowIcon
+                                        sx={{ fontSize: 30, color: "white" }}
+                                    />
+                                }
+                            </div>
+                        </div>
+                        <div style={{ marginLeft: 20 }}>
+                            <div style={{
+                                padding: "0 5px",
+                                background: "#333",
+                                fontSize: 30,
+                                width: "fit-content",
+                                color: "white"
+                            }}>
+                                Hỏi Dân IT's song
+                            </div>
+                            <div style={{
+                                padding: "0 5px",
+                                marginTop: 10,
+                                background: "#333",
+                                fontSize: 20,
+                                width: "fit-content",
+                                color: "white"
+                            }}
+                            >
+                                Eric
+                            </div>
+                        </div>
+                    </div>
+                    <div ref={containerRef} className="wave-form-container">
+                        <div className="time" >{time}</div>
+                        <div className="duration" >{duration}</div>
+                        <div ref={hoverRef} className="hover-wave"></div>
+                        <div className="overlay"
+                            style={{
+                                position: "absolute",
+                                height: "30px",
+                                width: "100%",
+                                bottom: "0",
+                                // background: "#ccc"
+                                backdropFilter: "brightness(0.5)"
+                            }}
+                        ></div>
 
+                    </div>
+                </div>
+                <div className="right"
+                    style={{
+                        width: "25%",
+                        padding: 15,
+                        display: "flex",
+                        alignItems: "center"
+                    }}
+                >
+                    <div style={{
+                        background: "#ccc",
+                        width: 250,
+                        height: 250
+                    }}>
+                    </div>
+                </div>
+            </div>
+        </Container >
     )
 }
 
